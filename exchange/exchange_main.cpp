115c2fb2212474653d9fb2a8f53410ec6f746268
Pranav Thatte <thattepranav@gmail.com>
2024-05-30 10:49:03 -0500

    updated main exchange file

--- exchange/exchange_main.cpp
+++ exchange/exchange_main.cpp
@@ -1,10 +1,14 @@
 #include <csignal>
 
-#include "./matching_engine/matching_engine.h"
+#include "matching_engine/matching_engine.h"
+#include "market_publisher/market_data_publisher.h"
+#include "order_gateway/order_server.h"
 #include "../utils/exchange_limits.h"
 
 Common::Logger *logger = nullptr;
 Exchange::MatchingEngine *matching_engine = nullptr;
+Exchange::MarketDataPublisher *market_data_publisher = nullptr;
+Exchange::OrderServer *order_server = nullptr;
 
 void signal_handler(int) {
     using namespace std::literals::chrono_literals;
@@ -15,6 +19,12 @@ void signal_handler(int) {
 
     delete matching_engine;
     matching_engine = nullptr;
+
+    delete market_data_publisher;
+    market_data_publisher = nullptr;
+
+    delete order_server;
+    order_server = nullptr;
 
     std::this_thread::sleep_for(10s);
 
@@ -40,14 +50,45 @@ int main() {
         Common::getCurrentTimeStr(&time_string)
     );
 
+    // starting the matching engine
     matching_engine = new Exchange::MatchingEngine(&client_requests, &client_responses, &market_updates); 
     matching_engine->start();
 
-    // dummy code that makes this sleep forever, will change this during testing
-    while(true) {
-        logger->log("%:% %() % Sleeping for now... \n",
-            __FILE__, __LINE__, __FUNCTION__,
-            Common::getCurrentTimeStr(&time_string)
+    // starting the publisher server
+    const std::string mkt_publisher_interface = "lo";
+    const std::string snapshot_publisher_ip = "233.252.14.1", inc_publisher_ip = "233.252.14.3";
+    const int snapshot_publisher_port = 20000, inc_publisher_port = 20001;
+
+    logger->log("%:% %() % Starting Publisher... \n",
+        __FILE__, __LINE__, __FUNCTION__,
+        Common::getCurrentTimeStr(&time_string)
+    );
+    market_data_publisher = new Exchange::MarketDataPublisher(
+        &market_updates, mkt_publisher_interface,
+        snapshot_publisher_ip, snapshot_publisher_port,
+        inc_publisher_ip, inc_publisher_port
+    );
+    market_data_publisher->start();
+
+    // starting the order server
+    const std::string order_gateway_interface = "lo";
+    const int order_gateway_port = 12345;
+
+    logger->log("%:% %() % Starting Gateway... \n",
+        __FILE__, __LINE__, __FUNCTION__,
+        Common::getCurrentTimeStr(&time_string)
+    );
+    order_server = new Exchange::OrderServer(
+        &client_requests, &client_responses, 
+        order_gateway_interface, order_gateway_port
+    );
+    order_server->start();
+
+    // ---------
+    // making this code run until it is explicetely killed by the user
+    while (true) {
+        logger->log("%:% %() % Keeping exchange alive... \n",
+            __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_string)
         );
         usleep(sleep_time * 1000);
     }
