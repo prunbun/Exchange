63afa25df0fe6a6f61d5044d47b71403554b3815
Pranav Thatte <thattepranav@gmail.com>
2024-11-12 22:47:04 -0600

    recv function finished to process incoming socket data

--- trading/market_data/market_data_consumer.cpp
+++ trading/market_data/market_data_consumer.cpp
@@ -47,6 +47,9 @@ namespace Trading {
 
     // so remember that the way a callback works is that the socket that calls this function is the 
     // argument to the function itself (hard to understand at first!)
+    // when we get data on a socket we are listening to, this function will be called
+    // when in recovery, we will start/continue that process
+    // else we will extract the MEMarketUpdate and send it to the client order book
     void MarketDataConsumer::recvCallback(MulticastSocket *socket) noexcept {
 
         // we need to first determine if we are receiving snapshot recovery data or an incremental update
@@ -64,9 +67,61 @@ namespace Trading {
             return;
         }
 
-        
+        // otherwise lets read the data
+        if (socket->next_receive_valid_index >= sizeof(Exchange::MDPMarketUpdate)) {
+            size_t i = 0;
+            for (; i + sizeof(Exchange::MDPMarketUpdate) <= socket->next_receive_valid_index; i += sizeof(Exchange::MDPMarketUpdate)) {
+                auto request = reinterpret_cast<const Exchange::MDPMarketUpdate *>(socket->inbound_data.data() + i);
 
-    
+                logger.log("%:% %() % Received % socket len:% %\n",
+                    __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str),
+                    (is_snapshot ? "snapshot" : "incremental"), sizeof(Exchange::MDPMarketUpdate),
+                    request->toString()
+                );
+
+                // so it is possible we are already in recovery OR we saw a gap in the sequence nums
+                const bool already_in_recovery = in_recovery;
+                in_recovery = (already_in_recovery || request->seq_number != next_exp_inc_seq_num);
+
+                if (UNLIKELY(in_recovery)) {
+
+                    // if we were not in recovery before and we just saw a mismatch seq num
+                    // we now have to activate recovery mode and listen to the snapshot broadcast
+                    if(UNLIKELY(!already_in_recovery)) {
+                        logger.log("%:% %() % Packet drops on % socket! SeqNum expected:% received:% \n",
+                            __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str),
+                            (is_snapshot ? "snapshot" : "incremental"), next_exp_inc_seq_num, request->seq_number
+                        );
+
+                        // begin recovery phase - subscribe to snapshot multicast
+                        startSnapshotSync();
+                    }
+
+                    // we'll send this message to processing, it could be snapshot OR incremental data
+                    queueMessage(is_snapshot, request);
+
+                } else if (!is_snapshot) { 
+                    // only care if it is incremental updates
+                    // note this is 'normal' operation when not in recovery
+
+                    // acknowledge that we got an incremental update
+                    logger.log("%:% %() % Incremental Request: %\n",
+                        __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str),
+                        request->toString()
+                    );
+                    ++next_exp_inc_seq_num;
+
+                    // great! let's send it to the client's order book
+                    Exchange::MEMarketUpdate * next_write = incoming_md_updates->getNextWriteTo();
+                    *next_write = std::move(request->me_market_update);
+                    incoming_md_updates->updateWriteIndex();
+                }
+            }
+
+            // let's update the socket's state based on how much we data we read
+            memcpy(socket->inbound_data.data(), socket->inbound_data.data() + i, socket->next_receive_valid_index - i);
+            socket->next_receive_valid_index -= i
+        }
     }
 
 }
