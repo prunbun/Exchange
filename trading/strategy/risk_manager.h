a1d971006ae718c213dd80ecb0ae243d1cb6705b
Pranav Thatte <thattepranav@gmail.com>
2025-01-17 12:50:57 -0600

    finished RiskInfo struct

--- trading/strategy/risk_manager.h
+++ trading/strategy/risk_manager.h
@@ -1,12 +1,80 @@
 #pragma once
 
+#include <sstream>
+
 #include "utils/macros.h"
-#include 
+#include "utils/logger.h"
+
+#include "position_keeper.h"
+#include "om_order.h"
+#include "utils/exchange_limits.h"
 
 
 using namespace Common;
 
 namespace Trading {
+
+    class OrderManager;
+
+    enum class RiskCheckResult : int8_t {
+        INVALID = 0,
+        ORDER_TOO_LARGE = 1,
+        POSITION_TOO_LARGE = 2,
+        LOSS_TOO_LARGE = 3,
+        ALLOWED = 4
+    };
+
+    inline std::string riskCheckResultToString(RiskCheckResult result) {
+
+        switch (result) {
+
+            case RiskCheckResult::INVALID: {return "INVALID";}
+            case RiskCheckResult::ORDER_TOO_LARGE: {return "ORDER_TOO_LARGE";}
+            case RiskCheckResult::POSITION_TOO_LARGE: {return "POSITION_TOO_LARGE";}
+            case RiskCheckResult::LOSS_TOO_LARGE: {return "LOSS_TOO_LARGE";}
+            case RiskCheckResult::ALLOWED: {return "ALLOWED";}
+
+            return "";
+        }
+
+    }
+
+    struct RiskInfo {
+
+        const PositionInfo *position_info = nullptr;
+        RiskConfig risk_cfg;
+
+        std::string toString() const  {
+            std::stringstream ss;
+
+            ss << "RiskInfo" << "[ "
+                << "pos: " << position_info->toString() << " "
+                << risk_cfg.toString() 
+                << "]";
+
+            return ss.str();
+        }
+
+        RiskCheckResult checkPreTradeRisk(Side side, Qty qty) const noexcept {
+
+            if (UNLIKELY(qty > risk_cfg.max_order_size)) {
+                return RiskCheckResult::ORDER_TOO_LARGE;
+            }
+
+            if (UNLIKELY(std::abs(position_info->position + sideToValue(side) * static_cast<int32_t>(qty)) > static_cast<int32_t>(risk_cfg.max_position))) {
+                return RiskCheckResult::POSITION_TOO_LARGE;
+            }
+
+            if (UNLIKELY(position_info->total_pnl < risk_cfg.max_loss)) {
+                return RiskCheckResult::LOSS_TOO_LARGE;
+            }
+
+            return RiskCheckResult::ALLOWED;
+        }
+    };
+
+    typedef std::array<RiskInfo, ME_MAX_TICKERS> TickerRiskInfoHashMap;
+
 
 
 
