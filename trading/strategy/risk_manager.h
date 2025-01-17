4a8421c8a30b59099addc86493f880d24fedc780
Pranav Thatte <thattepranav@gmail.com>
2025-01-17 12:22:08 -0600

    moved risk structs to utils types

--- trading/strategy/risk_manager.h
+++ trading/strategy/risk_manager.h
@@ -1,53 +1,14 @@
 #pragma once
 
-#include "utils/orderinfo_types.h"
-#include "utils/exchange_limits.h"
-
-#include <sstream>
+#include "utils/macros.h"
+#include 
 
 
 using namespace Common;
 
 namespace Trading {
 
-    // each ticker will have a risk configuration associated with it
-    struct RiskConfig {
-        Qty max_order_size = 0;
-        Qty max_position = 0;
-        double max_loss = 0;
 
-        std::string toString() const {
-            std::stringstream ss;
-
-            ss << "RiskConfig { "
-                << "max-order-size: " << qtyToString(max_order_size) << " "
-                << "max-position: " << qtyToString(max_position) << " "
-                << "max-loss: " << max_loss 
-                << " }";
-
-            return ss.str();
-        }
-    };
-
-    struct TradeEngineConfig {
-        Qty trade_size = 0;
-        double feature_threshold = 0;
-        RiskConfig risk_cfg;
-
-        std::string toString() const {
-            std::stringstream ss;
-
-            ss << "TradeEngineConfig { "
-                << "trade-size: " << qtyToString(trade_size) << " "
-                << "feature-threshold: " << feature_threshold << " "
-                << "risk config: " << risk_cfg.toString() << " "
-                << "}";
-
-            return ss.str();
-        }
-    };
-
-    typedef std::array<TradeEngineConfig, ME_MAX_TICKERS> TradeEngineConfigHashmap;
 
 }
 
--- utils/orderinfo_types.h
+++ utils/orderinfo_types.h
@@ -2,7 +2,8 @@
 
 #include <cstdint>
 #include <limits>
-#include "../utils/macros.h"
+#include "utils/macros.h"
+#include "utils/exchange_limits.h"
 
 namespace Common {
     // order id
@@ -108,4 +109,43 @@ namespace Common {
     inline constexpr int sideToValue(Side side) noexcept {
         return static_cast<int>(side);
     }
+
+    // each ticker will have a risk configuration associated with it
+    struct RiskConfig {
+        Qty max_order_size = 0;
+        Qty max_position = 0;
+        double max_loss = 0;
+
+        std::string toString() const {
+            std::stringstream ss;
+
+            ss << "RiskConfig { "
+                << "max-order-size: " << qtyToString(max_order_size) << " "
+                << "max-position: " << qtyToString(max_position) << " "
+                << "max-loss: " << max_loss 
+                << " }";
+
+            return ss.str();
+        }
+    };
+
+    struct TradeEngineConfig {
+        Qty trade_size = 0;
+        double feature_threshold = 0;
+        RiskConfig risk_cfg;
+
+        std::string toString() const {
+            std::stringstream ss;
+
+            ss << "TradeEngineConfig { "
+                << "trade-size: " << qtyToString(trade_size) << " "
+                << "feature-threshold: " << feature_threshold << " "
+                << "risk config: " << risk_cfg.toString() << " "
+                << "}";
+
+            return ss.str();
+        }
+    };
+
+    typedef std::array<TradeEngineConfig, ME_MAX_TICKERS> TradeEngineConfigHashmap;
 }
